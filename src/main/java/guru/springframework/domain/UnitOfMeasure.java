837d0f856a83397294c1e5b1e5cd7bee028fffae
Marc Fearby <marc.fearby@service.nsw.gov.au>
2022-07-14 21:14:59 +1000

    Rename property in line with change made in the solution

--- src/main/java/guru/springframework/domain/UnitOfMeasure.java
+++ src/main/java/guru/springframework/domain/UnitOfMeasure.java
@@ -9,17 +9,17 @@ public class UnitOfMeasure {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long Id;
 
-    private String uom;
+    private String description;
 
     public Long getId() {
         return Id;
     }
 
-    public String getUom() {
-        return uom;
+    public String getDescription() {
+        return description;
     }
 
-    public void setUom(String uom) {
-        this.uom = uom;
+    public void setDescription(String uom) {
+        this.description = uom;
     }
 }
