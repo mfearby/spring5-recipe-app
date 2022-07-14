fcb4ada73346812c9851b5ea293ae64a1db92a50
Marc Fearby <marc.fearby@service.nsw.gov.au>
2022-07-14 21:28:31 +1000

    Add difficulty enumeration and getters/setters for ingredients set previously forgotton

--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -28,6 +28,9 @@ public class Recipe {
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
     private Set<Ingredient> ingredients;
 
+    @Enumerated(EnumType.STRING)
+    private Difficulty difficulty;
+
 
     public Long getId() {
         return id;
@@ -103,5 +106,21 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
     }
 }
